5167298ebb622e9ec34a7ec38426fb25340b18ab
yfsteven <152656884+yfsteven@users.noreply.github.com>
2024-03-08 18:41:46 -0500

    Added controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,34 @@
+const Category = require("../models/Category");
+const asyncHandler = require("express-async-handler");
+
+exports.category_list = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category list")
+});
+
+exports.category_detail = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category detail")
+});
+
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category create get")
+});
+
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category create post")
+});
+
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category delete get")
+});
+
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category delete post")
+});
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category update get")
+});
+
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented category update post")
+});
---
+++ controllers/videogameController.js
@@ -0,0 +1,34 @@
+const VideoGame = require("../models/VideoGame");
+const asyncHandler = require("express-async-handler");
+
+exports.videogame_list = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg list")
+});
+
+exports.videogame_detail = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg detail")
+});
+
+exports.videogame_create_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg create get")
+});
+
+exports.videogame_create_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg create post")
+});
+
+exports.videogame_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg delete get")
+});
+
+exports.videogame_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg delete post")
+});
+
+exports.videogame_update_get = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg update get")
+});
+
+exports.videogame_update_post = asyncHandler(async (req, res, next) => {
+    res.send("Not implemented vg update post")
+});

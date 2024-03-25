49d7b189ff4dd808ed25c978168196acaea18647
yfsteven <152656884+yfsteven@users.noreply.github.com>
2024-03-24 21:59:36 -0400

    Displayed videogame list

--- controllers/videogameController.js
+++ controllers/videogameController.js
@@ -1,14 +1,57 @@
 const VideoGame = require("../models/videogame");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const category = require("../models/category");
+
 
 exports.index = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented Homepage")
+    const [
+        numGames
+    ] = await Promise.all([
+        VideoGame.countDocuments({}).exec(),
+    ]);
+
+    const allCategories = await Category.find().sort({ title: 1}).exec();
+
+    let platforms = "";
+
+    for(i = 0; i < allCategories.length; i++) {
+        if(i != allCategories.length - 1) {
+            platforms += allCategories[i].title + ', ';
+        } else {
+            platforms += 'and ' + allCategories[i].title;
+        }
+    }
+
+    res.render("index", {
+        title: "Videogame Inventory Application",
+        videogame_count: numGames,
+        category_title: platforms,
+    });
 });
 
 exports.videogame_list = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented vg list")
-});
+    const allVideogames = await VideoGame.find({}, "title description category numberstocks price")
+    .sort({ title: 1 })
+    .populate("description")
+    .populate("category")
+    .populate("numberstocks")
+    .populate("price")
+    .exec();
 
+    
+    res.render("videogame_list", { title: "Video Games", videogame_list: allVideogames});
+});
+/*
+const allVideogames = await VideoGame.find({}, "title description category numberstocks price")
+    .sort({ title: 1 })
+    .populate("description")
+    .populate("category")
+    .populate("numberstocks")
+    .populate("price")
+    .exec();
+    
+console.log(allVideogames.description)*/
 exports.videogame_detail = asyncHandler(async (req, res, next) => {
     res.send("Not implemented vg detail")
 });
--- models/videogame.js
+++ models/videogame.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const VideoGameSchema = new Schema({
     title: { type: String, required: true, minLength: 3, maxLength: 100 },
     description: { type: String, required: true },
-    category: [ {type: Schema.Types.ObjectId, ref: "Category"} ],
+    category: [ {type: Schema.Types.ObjectId, required: true, ref: "Category"}],
     numberstocks: { type: Number, required: true},
     price: { type: Number, required: true },
 });
@@ -13,5 +13,8 @@ const VideoGameSchema = new Schema({
 VideoGameSchema.virtual('url').get(function () {
     return `/videogame/${this._id}`;
 });
+
+
+
 
 module.exports = mongoose.model('VideoGame', VideoGameSchema);
